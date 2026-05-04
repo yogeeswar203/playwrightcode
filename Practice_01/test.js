97ff9c3e6f434976d0de283146908556a8cf5347
Yogeeswar Achari <yogeeswar203@gmail.com>
2026-05-05 00:42:44 +0530

    1st commit from java script

---
+++ Practice_01/test.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("This is my 1st program in type script");
---
+++ Practice_01/test.ts
@@ -0,0 +1,3 @@
+//console.log("This is my 1st program in type script");
+var age = 30;
+console.log("Age: "+age);
