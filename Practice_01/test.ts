8971cd83dcefc20c75204f288a8a0dfaab8d1b5c
Yogeeswar Achari <yogeeswar203@gmail.com>
2026-05-05 00:56:11 +0530

    let var and const variables

--- Practice_01/test.ts
+++ Practice_01/test.ts
@@ -2,3 +2,30 @@
 var age = 30;
 console.log("Age: "+age);
 // Testing the JS file
+
+function m1()
+{
+    if(true)
+    {
+        var msg = "This is JS Progam";
+        //console.log(msg);
+    }
+
+    console.log(msg);
+}
+
+m1();
+
+function m2()
+{
+    if(true)
+    {
+        let msg = "method 2";
+        const msg2 = "Hellow world";
+        console.log(msg);
+         console.log(msg2);
+    }
+
+    //console.log(msg);
+}
+m2();
