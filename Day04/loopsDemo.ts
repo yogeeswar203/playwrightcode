856d5b38e4edad88e9f454be3afac9ed2d3950ee
Yogeeswar Achari <yogeeswar203@gmail.com>
2026-05-21 21:50:32 +0530

    added loop practice program

---
+++ Day04/loopsDemo.ts
@@ -0,0 +1,44 @@
+//Examples for the loop
+
+/*
+while(condition)
+{
+    statements; (increments/decrements)
+    
+}
+
+*/
+
+
+
+// while(i<=5)
+// {
+//     console.log(`${i} `)
+//     i++;
+// }
+
+
+// while(i<=9)
+// {   
+//     console.log(i);
+//     i+=2;
+// }
+
+
+// while(i>=1)
+// {
+//     console.log(`${i}`)
+//     i-=2;
+// }
+
+
+let i:number=10;
+for(i=1; i<=5; i++)
+{
+    console.log(`${i}`)
+}
+
+console.log(i);
+
+for(i=2; i<20; i++);
+console.log(i);
