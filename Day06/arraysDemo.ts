678da8bcc621428f327a5affbd03afbef7ec6cf5
Yogeeswar Achari <yogeeswar203@gmail.com>
2026-07-18 22:56:23 +0530

    added arrays programs

--- Day06/arraysDemo.ts
+++ Day06/arraysDemo.ts
@@ -16,3 +16,47 @@ console.log(names);
 
 let array2:Array<string | number> = ['ra', 'ka', 1,2];
 console.log(array2);
+
+let array3:Array<any> = [1, "test", "Raha", true, null]
+console.log(array3);
+console.log(array3[0]);
+console.log(array3[1]);
+let i:number;
+
+
+for(i=0; i<array2.length;i++)
+{
+    console.log(array2[i]);
+}
+
+// for in loop example
+console.log("Printing the data from the array using for in Loop")
+for(let i in array2)
+{
+    console.log(array2[i]);
+}
+
+console.log("Printing the datausing for of Loop")
+
+for(let value of array3)
+{
+    console.log(value);
+}
+
+
+function search(el:number, arr:number[]):boolean
+{
+    for(let i=0; i<arr.length; i++)
+    {
+        if(arr[i]===el)
+        {
+            return true;
+        }
+    }
+    return true;
+}
+
+let arr3:number[] = [1,2,3]
+
+console.log("printing the data using the function")
+console.log(search(1, arr3));
