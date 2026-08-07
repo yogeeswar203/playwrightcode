83f7d1e4a13672eaff0e2c2b14f820ad2a04e889
Yogeeswar Achari <yogeeswar203@gmail.com>
2026-08-07 17:29:31 +0530

    added programs for map reduce

--- Day06/arraymenthods2.ts
+++ Day06/arraymenthods2.ts
@@ -25,4 +25,57 @@ fruits.forEach((ta, id)=>{
 
 fruits.forEach((element)=>{
     console.log(`${element.toUpperCase()}`);
-});
+})
+
+
+// exmples for the map function 
+// map creates a new array with the result of calling the function on every element of an array 
+// It takes fucntion as a parameter
+// Returns the same number of elements that we have in original array
+// Syntax array.map(function(currentValue, index, array){})
+
+// ex1: get squar of all the numbers in an array. Ex[1,2,3] then result should be [1, 4, 9]
+
+
+
+let numbers:number[] =[1,2,3,4];
+
+let sqnumbers = numbers.map(function(e1){return(e1*e1);});
+console.log(sqnumbers);
+
+let addNum=numbers.map(function(e1){return(e1+e1);});
+console.log(addNum);
+
+// using arrow function
+
+let addNum2 = numbers.map((e1)=>{return(e1+e1);});
+console.log(addNum2);
+
+// if you have single return statement then {} and return statement are optional
+
+let addnum3 = numbers.map((e1)=>e1*2);
+console.log(addnum3);
+
+
+// filter() - creates a new array with all the elements that pass/satisfy the function
+// it takes the function as a parameter
+// Returns wither same or fewr number of elements compared to original array
+// Syntax array/filter(function(currentValue, index, array){})
+
+// ex:1 get only even numbers from the given array 
+
+let halNum = numbers.filter((num)=>{
+    return(num%2===0);
+})
+console.log(halNum);
+
+
+let hls = numbers.filter((e1)=>(e1%2!==0));
+console.log(hls);
+
+
+let hg = numbers.filter((e1)=>(e1>3));
+console.log(hg);
+
+let hg1 = numbers.filter((e1)=>{return(e1>2)});
+console.log(hg1);
